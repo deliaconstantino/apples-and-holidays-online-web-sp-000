3843905407045435a43877feadecf0552ec7b564
Delia Constantino <dconstan92@gmail.com>
2020-11-02 19:57:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_holidays_with_bbq(holiday_hash)
   holidays_w_bbq = []
   holiday_hash.each do |season, holiday_info_hash|
     if holiday_info_hash.values.flatten.include?("BBQ")
-    holidays_w_bbq << holiday_info_hash.keys
+    holidays_w_bbq << holiday_info_hash.keys.join
     end
   end
   holidays_w_bbq
